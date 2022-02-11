7723da63bea2f451252dbca74c66ad71fcc4708e
蔡显麒 <583304356@qq.com>
2022-02-11 23:13:15 +0800

    更新 添加user实体类

---
+++ Domain/Models/User/UserEntity.cs
@@ -0,0 +1,40 @@
+﻿using Share.Helper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Domain.Models.User
+{
+    [Table("user")]
+    [Description("用户")]
+    public class UserEntity:BaseEntity<Guid>
+    {
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        [Column("username")]
+        [Required]
+        [Description("用户名")]
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 密码（不映射）
+        /// </summary>
+        [Required]
+        [NotMapped]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 映射密码
+        /// </summary>
+        [Column("password")]
+        [Required]
+        [Description("密码")]
+        private string EncryptPassword => MD5Helper.GenerateMD5(Password);
+
+
+    }
+}
---
+++ Share/Helper/MD5Helper.cs
@@ -0,0 +1,46 @@
+﻿
+using Extensions.ServiceConfigures.AppSettings;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+
+namespace Share.Helper
+{
+    public static class MD5Helper
+    {
+        public static string secretKey;
+
+        static MD5Helper()
+        {
+            secretKey=Appsettings.app(new string[] { "AppSettings", "MD5SecretKey" });
+        }
+
+        /// <summary>
+        /// MD5字符串加密
+        /// </summary>
+        /// <param name="txt"></param>
+        /// <returns>加密后字符串</returns>
+        public static string GenerateMD5(string txt)
+        {
+            using (MD5 mi = MD5.Create())
+            {
+                byte[] buffer = Encoding.Default.GetBytes(txt+ secretKey);
+                //开始加密
+                byte[] newBuffer = mi.ComputeHash(buffer);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < newBuffer.Length; i++)
+                {
+                    sb.Append(newBuffer[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+
+}
